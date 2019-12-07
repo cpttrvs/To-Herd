8df3dbe0af921d8876fa004cde41ca033bb83117
cpttrvs <cpt_trivius@hotmail.fr>
2019-12-07 16:43:42 +0100

    before pivoting to new concept

---
+++ Assets/Components/Wolf/WolfSelector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class WolfSelector : MonoBehaviour
+{
+    public bool isSelected = false;
+
+    public Action<WolfSelector> OnSelection;
+    public Action<WolfSelector> OnDeselection;
+
+    public Action<Vector3> OnMoveOrder;
+
+    [SerializeField]
+    private WolfController _controller = null;
+
+    public void Select()
+    {
+        //Debug.Log("SELECTED");
+        isSelected = true;
+        OnSelection?.Invoke(this);
+    }
+
+    public void Deselect()
+    {
+        //Debug.Log("DESELECTED");
+        isSelected = false;
+        OnDeselection?.Invoke(this);
+    }
+
+    public void Move(Vector3 pos)
+    {
+        //Debug.Log("MOVE:" + pos);
+        OnMoveOrder?.Invoke(pos);
+    }
+
+    public WolfController GetController() { return _controller; }
+}
