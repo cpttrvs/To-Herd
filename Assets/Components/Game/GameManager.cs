94162436d11f611a270d69dd4db5e5985b37884e
cpttrvs <cpt_trivius@hotmail.fr>
2019-12-07 14:59:33 +0100

    starting environment

--- Assets/Components/Game/GameManager.cs
+++ Assets/Components/Game/GameManager.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject monolithPrefab = null;
+
+    private Monolith monolith = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(monolithPrefab != null)
+        {
+            monolith = monolithPrefab.GetComponentInChildren<Monolith>();
+
+            if(monolith != null)
+            {
+                monolith.OnSheepEnter += Monolith_OnSheepEnter;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        if (monolith != null)
+        {
+            monolith.OnSheepEnter -= Monolith_OnSheepEnter;
+        }
+    }
+
+    void Monolith_OnSheepEnter()
+    {
+        // end game
     }
 }
---
+++ Assets/Components/Monolith/Monolith.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Monolith : MonoBehaviour
+{
+    public Action OnSheepEnter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Sheep"))
+        {
+            Debug.Log("[Monolith] Sheep entered");
+            OnSheepEnter?.Invoke();
+        }
+    }
+}
