57c1420c09d992976fe6b42e31112bf809aff569
cpttrvs <cpt_trivius@hotmail.fr>
2019-12-08 14:13:41 +0100

    intro menu (wip)

---
+++ Assets/Components/UI/Menu/UIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private string gameSceneName = "";
+
+    [SerializeField]
+    private Button playButton = null;
+    [SerializeField]
+    private Button settingsButton = null;
+    [SerializeField]
+    private Button quitButton = null;
+
+    private void Start()
+    {
+        playButton.onClick.AddListener(OnPlayClick);
+        settingsButton.onClick.AddListener(OnSettingsClick);
+        quitButton.onClick.AddListener(OnQuitClick);
+    }
+
+    private void OnDestroy()
+    {
+        playButton.onClick.RemoveListener(OnPlayClick);
+        settingsButton.onClick.RemoveListener(OnSettingsClick);
+        quitButton.onClick.RemoveListener(OnQuitClick);
+    }
+
+    void OnPlayClick()
+    {
+        SceneManager.LoadScene(gameSceneName, LoadSceneMode.Single);
+    }
+
+    void OnSettingsClick()
+    {
+
+    }
+
+    void OnQuitClick()
+    {
+        Application.Quit();
+    }
+}
